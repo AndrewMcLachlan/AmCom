61985d5e7c41b42966c885bce9d2d6880f9dd54e
Andrew McLachlan <amc.home@outlook.com>
2019-03-21 13:00:35 +1000

    Added webpack compilation and included PoC on the regex page.

---
+++ src/Asm.AmCom.Web/webpack.config.js
@@ -0,0 +1,20 @@
+﻿const path = require("path");
+
+module.exports = {
+  entry: "./scripts/react/app.js",
+  output: {
+    path: path.join(__dirname, "/wwwroot/js"),
+    filename: "regex.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        },
+      }
+    ]
+  }
+};
