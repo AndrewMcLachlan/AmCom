c46d88f9e85ad408bd7f92f11e1b06b8293ac7dd
Andrew McLachlan <amc.home@outlook.com>
2019-03-27 17:18:13 +1000

    Added API for calculating CIDR notation.

---
+++ src/Asm.AmCom.Web/Areas/Tools/Models/CidrModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Asm.AmCom.Web.Areas.Tools.Models
+{
+    public class CidrRequest
+    {
+        public IPAddress IPAddress {get; set;}
+
+        public IPAddress SubnetMask {get; set;}
+    }
+
+    public class CidrResponse
+    {
+        public string IPAddress {get; set;}
+    }
+}
