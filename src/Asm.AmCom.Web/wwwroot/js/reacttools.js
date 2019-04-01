c1998a29e722bfa7dddcc0ca04c6c152777d3e43
Andrew McLachlan <amc.home@outlook.com>
2019-04-02 08:30:44 +1000

    Compiled React code.

--- src/Asm.AmCom.Web/wwwroot/js/reacttools.js
+++ src/Asm.AmCom.Web/wwwroot/js/reacttools.js
@@ -1458,6 +1458,136 @@ var cidr;
 
 /***/ }),
 
+/***/ "./scripts/react/Tools/Base64.tsx":
+/*!****************************************!*\
+  !*** ./scripts/react/Tools/Base64.tsx ***!
+  \****************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "default", function() { return Base64; });
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "react");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+
+
+var Base64 =
+/*#__PURE__*/
+function (_React$Component) {
+  _inherits(Base64, _React$Component);
+
+  function Base64(props) {
+    var _this;
+
+    _classCallCheck(this, Base64);
+
+    _this = _possibleConstructorReturn(this, _getPrototypeOf(Base64).call(this, props));
+
+    _this.xCode = function (e) {
+      var resultValue;
+
+      try {
+        resultValue = _this.b64DecodeUnicode(_this.state.input);
+      } catch (e) {
+        resultValue = _this.b64EncodeUnicode(_this.state.input);
+      }
+
+      _this.setState({
+        output: resultValue.toString()
+      });
+    };
+
+    _this.b64EncodeUnicode = function (str) {
+      return btoa(encodeURIComponent(str).replace(/%([0-9A-F]{2})/g, function (_, p1) {
+        return String.fromCharCode(+('0x' + p1));
+      }));
+    };
+
+    _this.b64DecodeUnicode = function (str) {
+      return decodeURIComponent(Array.prototype.map.call(atob(str), function (c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+      }).join(''));
+    };
+
+    _this.state = {
+      input: props.input
+    };
+    return _this;
+  }
+
+  _createClass(Base64, [{
+    key: "render",
+    value: function render() {
+      var _this2 = this;
+
+      return react__WEBPACK_IMPORTED_MODULE_0__["createElement"]("div", null, react__WEBPACK_IMPORTED_MODULE_0__["createElement"]("section", {
+        className: "row"
+      }, react__WEBPACK_IMPORTED_MODULE_0__["createElement"]("div", {
+        className: "col-md-9"
+      }, react__WEBPACK_IMPORTED_MODULE_0__["createElement"]("fieldset", null, react__WEBPACK_IMPORTED_MODULE_0__["createElement"]("legend", {
+        className: "sr-only"
+      }, "Base 64 Encode / Decode"), react__WEBPACK_IMPORTED_MODULE_0__["createElement"]("div", {
+        className: "form-group"
+      }, react__WEBPACK_IMPORTED_MODULE_0__["createElement"]("label", {
+        htmlFor: "theString",
+        className: "control-label"
+      }, "String to encode / decode"), react__WEBPACK_IMPORTED_MODULE_0__["createElement"]("textarea", {
+        className: "form-control",
+        id: "source",
+        spellCheck: false,
+        onChange: function onChange(e) {
+          return _this2.setState({
+            input: e.currentTarget.value
+          });
+        }
+      })), react__WEBPACK_IMPORTED_MODULE_0__["createElement"]("button", {
+        className: "btn btn-primary mb-3",
+        id: "encodeDecode",
+        onClick: this.xCode
+      }, "Encode / Decode")))), react__WEBPACK_IMPORTED_MODULE_0__["createElement"]("section", {
+        className: "row"
+      }, react__WEBPACK_IMPORTED_MODULE_0__["createElement"]("div", {
+        className: "col-md-9"
+      }, react__WEBPACK_IMPORTED_MODULE_0__["createElement"]("div", {
+        className: "form-group"
+      }, react__WEBPACK_IMPORTED_MODULE_0__["createElement"]("label", {
+        htmlFor: "result",
+        className: "control-label"
+      }, "Result"), react__WEBPACK_IMPORTED_MODULE_0__["createElement"]("textarea", {
+        className: "form-control",
+        id: "result",
+        spellCheck: false,
+        value: this.state.output
+      })))));
+    }
+  }]);
+
+  return Base64;
+}(react__WEBPACK_IMPORTED_MODULE_0__["Component"]);
+
+
+
+/***/ }),
+
 /***/ "./scripts/react/Tools/Cidr.tsx":
 /*!**************************************!*\
   !*** ./scripts/react/Tools/Cidr.tsx ***!
@@ -1923,6 +2053,8 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _Redux_Reducers__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../Redux/Reducers */ "./scripts/react/Redux/Reducers.ts");
 /* harmony import */ var _Regex__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./Regex */ "./scripts/react/Tools/Regex.tsx");
 /* harmony import */ var _Cidr__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./Cidr */ "./scripts/react/Tools/Cidr.tsx");
+/* harmony import */ var _Base64__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./Base64 */ "./scripts/react/Tools/Base64.tsx");
+
 
 
 
@@ -1936,6 +2068,11 @@ var tools = [{
   initialState: _Redux_Reducers__WEBPACK_IMPORTED_MODULE_0__["cidr"].initialState,
   reducer: _Redux_Reducers__WEBPACK_IMPORTED_MODULE_0__["cidr"].reducer,
   component: _Cidr__WEBPACK_IMPORTED_MODULE_2__["default"]
+}, {
+  name: "base64",
+  initialState: {},
+  reducer: function reducer() {},
+  component: _Base64__WEBPACK_IMPORTED_MODULE_3__["default"]
 }];
 
 /***/ }),
