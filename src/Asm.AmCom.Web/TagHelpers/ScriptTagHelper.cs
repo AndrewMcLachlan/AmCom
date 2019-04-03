fd4f70931be73535b1d6fd88976d9389d0211c0e
Andrew McLachlan <amc.home@outlook.com>
2019-04-03 16:07:51 +1000

    Fixed ScriptTagHelper.

--- src/Asm.AmCom.Web/TagHelpers/ScriptTagHelper.cs
+++ src/Asm.AmCom.Web/TagHelpers/ScriptTagHelper.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Html;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Mvc.Routing;
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
@@ -15,19 +17,29 @@ namespace Asm.AmCom.Web.TagHelpers
         [ViewContext]
         public ViewContext ViewContext { get; set; }
 
+        private IUrlHelper _urlHelper;
+
+        public ScriptTagHelper(IActionContextAccessor actionContextAccessor, IUrlHelperFactory urlHelperFactory)
+        {
+            _urlHelper = urlHelperFactory.GetUrlHelper(actionContextAccessor.ActionContext);
+        }
+
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
-            this.ViewContext.FormContext.
+            // Not a script with src-min
+            if (!context.AllAttributes.Any(a => a.Name == "src-min")) return;
+
             output.TagName = "script";
 #if DEBUG
-            object src = context.AllAttributes["src"]?.Value;
+            var src = context.AllAttributes["src"]?.Value as HtmlString;
 #else
-            object src = context.AllAttributes["src-min"]?.Value ?? context.AllAttributes["src"]?.Value;
+            var src = (context.AllAttributes["src-min"]?.Value ?? context.AllAttributes["src"]?.Value) as HtmlString;
 #endif
-            output.Attributes.RemoveAll("src-min");
 
-            //output.Attributes["src"].Value = new Microsoft.AspNetCore.Mvc.Razor.TagHelpers.UrlResolutionTagHelper.EncodeFirstSegmentContent
-         //       .Add(new TagHelperAttribute("src", src));
+            output.Attributes.RemoveAll("src-min");
+            output.Attributes.RemoveAll("src");
+
+            output.Attributes.Add(new TagHelperAttribute("src", _urlHelper.Content(src.Value)));
         }
     }
 }
