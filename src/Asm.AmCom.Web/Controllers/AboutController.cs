1f1080369a1e18b1c2be21fa5bc017d4a75ffaf6
Andrew McLachlan <amc.home@outlook.com>
2019-06-06 16:03:06 +1000

    Added support for proxying CV from Sync or another location.

--- src/Asm.AmCom.Web/Controllers/AboutController.cs
+++ src/Asm.AmCom.Web/Controllers/AboutController.cs
@@ -1,23 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
 using System.Web;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 
 namespace Asm.AmCom.Web.Controllers
 {
     public class AboutController : Controller
     {
+        private IConfiguration _configuration;
+
+        public AboutController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         [Route("about-me")]
         public ActionResult Index()
         {
             return View("Index");
         }
 
-        /*[ActionName(".*")]
-        public ActionResult Generic()
+        [Route("resume")]
+        public async Task<ActionResult> Resume()
         {
-            return View();
-        }*/
+            HttpClient client = new HttpClient();
+
+            Stream stream = await client.GetStreamAsync(_configuration["Resume"]);
+
+            return new FileStreamResult(stream, "application/pdf");
+        }
     }
 }
